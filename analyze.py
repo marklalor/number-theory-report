0aa401a5bdaf182d58b331fa578c4c05d391804f
Mark Lalor <markwlalor@gmail.com>
2018-12-07 15:18:13 -0500

    Add analysis for pixel graph and outlier detection.

--- analyze.py
+++ analyze.py
@@ -1,18 +1,57 @@
 import argparse
 import csv
 import json
-import sys
-from typing import Set, Dict, Tuple, List
+from typing import Dict, Tuple, List
+
+import numpy as np
+from scipy.misc import imsave
 
 
 def disparity_terms(l1,l2):
     return abs(len(l1) - len(l2))
 
+
 def disparity_denominator(l1,l2):
-    return max(l1) - max(l2)
+    return abs(len(str(max(l1))) - len(str(max(l2))))
+
 
 def do_pixel_terms(results: Dict[Tuple[int, int], Tuple[List[int], List[int]]]):
-    pass
+    do_pixel_graph(results, disparity_terms)
+
+
+def do_pixel_denominator(results: Dict[Tuple[int, int], Tuple[List[int], List[int]]]):
+    do_pixel_graph(results, disparity_denominator)
+
+
+def do_pixel_graph(results: Dict[Tuple[int, int], Tuple[List[int], List[int]]], disparity_function):
+    term_disparities = {key: disparity_function(*value) for key, value in results.items()}
+
+    max_disparity = max(term_disparities.values())
+    data = np.zeros((500, 500, 3), dtype=np.uint8)
+    for i in range(0, 500):
+        for j in range(0, 500):
+            data[i, j] = 255
+
+    color_step = 255/max_disparity
+
+    greys = {i: 255 - color_step*i for i in range(0, max_disparity+1)}
+
+    for point, disparity in term_disparities.items():
+        x, y = point
+        data[x-1, y-1] = greys[disparity]
+
+
+    imsave(f'pixel_{disparity_function.__name__}.png', data)
+
+def do_greedy_outliers(results: Dict[Tuple[int, int], Tuple[List[int], List[int]]]):
+    term_disparities = {key: disparity_denominator(*value) for key, value in results.items()}
+
+    max_disparity = max(term_disparities.values())
+
+    top_results = sorted(term_disparities.items(), key=lambda x: -x[1])[:10]
+
+    for point, disparity in top_results:
+        print(f'{point}: {disparity}')
 
 def do_constant_disp(results: Dict[Tuple[int, int], Tuple[List[int], List[int]]]):
     sums = {}
@@ -41,7 +80,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Make some graphs')
     parser.add_argument('result_file')
-    parser.add_argument('actions', nargs='+', choices={'pixel_terms', 'pixel_denominators', 'constant_disp'}, help='actions to perform')
+    parser.add_argument('actions', nargs='+', help='actions to perform')
     args = parser.parse_args()
 
     mapping = dict()

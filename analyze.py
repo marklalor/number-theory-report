c5b31d03449e5d784025a93472cf135de5d2c22b
Mark Lalor <markwlalor@gmail.com>
2018-12-07 13:52:26 -0500

    Fix analysis wrapper.

--- analyze.py
+++ analyze.py
@@ -18,30 +18,34 @@ def do_pixel_terms(results: Dict[Tuple[int, int], Result]):
 
 
 if __name__ == '__main__':
-    if len(sys.argv[1:]) == 0:
-        print("Usage: analyze.py [results_file] [image|]")
+    # if len(sys.argv[1:]) == 0:
+    #     print("Usage: analyze.py [results_file] [image|]")
 
-    result_file = sys.argv[1]
-    actions = sys.argv[2:]
+    # result_file = sys.argv[1]
+    # actions = sys.argv[2:]
+
+
+    parser = argparse.ArgumentParser(description='Make some graphs')
+    parser.add_argument('result_file')
+    parser.add_argument('actions', nargs='+', choices={'pixel_terms', 'pixel_denominators'}, help='actions to perform')
+    args = parser.parse_args()
 
     mapping = dict()
 
-    with open(result_file, 'r') as csvfile:
+    with open(args.result_file, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for row in reader:
             x_raw, y_raw, greedy_raw, brute_raw = row
             x = int(x_raw)
             y = int(y_raw)
-            greedy = json.loads(greedy_raw, separators=(',', ':'))
-            brute = json.loads(brute_raw, separators=(',', ':'))
+            greedy = json.loads(greedy_raw)
+            brute = json.loads(brute_raw)
 
             mapping[(x, y)] = (greedy, brute)
 
-    parser = argparse.ArgumentParser(description='Make some graphs')
-    parser.add_argument('actions', nargs='+', choices={'pixel_terms', 'pixel_terms'}, help='actions to perform')
-    for action in parser.parse_args().actions:
+    for action in args.actions:
         print(f'Running {action} action:')
-        method_to_call = globals()[f'do_{action}']()
+        method_to_call = globals()[f'do_{action}'](mapping)
         print(f'Finished {action} action!')
         print()
 
